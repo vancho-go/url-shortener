aa23c7328d3b3beaece1eec5717300830daf17db
Ivan Goncharov <goncharov.80@mail.ru>
2023-09-28 13:57:50 +0300

    Fixes after staticcheck

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -19,35 +19,35 @@ func mainPage(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	if req.Method == http.MethodGet {
-		shortenUrl := strings.TrimPrefix(req.URL.Path, "/")
-		if val, ok := dbInstance[shortenUrl]; !ok {
+		shortenURL := strings.TrimPrefix(req.URL.Path, "/")
+		if originalURL, ok := dbInstance[shortenURL]; !ok {
 			http.Error(res, "No such shorten URL", http.StatusBadRequest)
 			return
 		} else {
-			res.Header().Set("Location", val)
+			res.Header().Set("Location", originalURL)
 			res.WriteHeader(http.StatusTemporaryRedirect)
 		}
 	}
 
 	if req.Method == http.MethodPost {
-		url, err := io.ReadAll(req.Body)
+		originalURL, err := io.ReadAll(req.Body)
 		if err != nil {
 			_, _ = res.Write([]byte(err.Error()))
 			return
 		}
-		if string(url) == "" {
+		if string(originalURL) == "" {
 			http.Error(res, "URL parameter is missing", http.StatusBadRequest)
 			return
 		}
 		id := helpers.Base62Encode(rand.Uint64())
-		dbInstance[id] = string(url)
+		dbInstance[id] = string(originalURL)
 		res.WriteHeader(http.StatusCreated)
-		a, err := url2.JoinPath("http://", addr, id)
+		shortenURL, err := url2.JoinPath("http://", addr, id)
 		if err != nil {
 			_, _ = res.Write([]byte(err.Error()))
 			return
 		}
-		_, _ = res.Write([]byte(a))
+		_, _ = res.Write([]byte(shortenURL))
 	}
 
 }
