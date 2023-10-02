c8d8151344b354a8308238c407892024334abfa1
Ivan Goncharov <goncharov.80@mail.ru>
2023-10-02 19:14:57 +0300

    Added env vars

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var Configuration = new(Config)
 
@@ -13,4 +16,9 @@ func ParseFlags() {
 	flag.StringVar(&Configuration.ServerHost, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&Configuration.BaseHost, "b", "http://"+Configuration.ServerHost, "address and port for shorten URLs")
 	flag.Parse()
+
+	if envRunAddr, envBaseAddr := os.Getenv("SERVER_ADDRESS"), os.Getenv("BASE_URL"); envRunAddr != "" && envBaseAddr != "" {
+		Configuration.ServerHost = envRunAddr
+		Configuration.BaseHost = envBaseAddr
+	}
 }
