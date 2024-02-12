9cfc7063420902c294bce2d2b5753e91d5390a34
Ivan Goncharov <goncharov.80@mail.ru>
2024-02-12 21:04:51 +0300

    Changed print function

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -21,7 +21,7 @@ func main() {
 }
 
 func printBuildInfo() {
-	fmt.Println("Build version: %s", buildVersion)
-	fmt.Println("Build date: %s", buildDate)
-	fmt.Println("Build commit: %s", buildCommit)
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 }
