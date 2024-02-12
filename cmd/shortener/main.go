4a1e5b0b4f54871fba56194354126da18ffb8885
Ivan Goncharov <goncharov.80@mail.ru>
2024-02-12 21:03:50 +0300

    Added build info

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/vancho-go/url-shortener/internal/app"
 	"log"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	printBuildInfo()
+
 	if err := app.Run(); err != nil {
 		log.Panic(err.Error())
 	}
+}
+
+func printBuildInfo() {
+	fmt.Println("Build version: %s", buildVersion)
+	fmt.Println("Build date: %s", buildDate)
+	fmt.Println("Build commit: %s", buildCommit)
 }
