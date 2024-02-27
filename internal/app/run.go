791379204972fd117c62ec967e2129ec91e3df35
Ivan Goncharov <goncharov.80@mail.ru>
2024-02-27 20:05:21 +0300

    Added server shutdown

--- internal/app/run.go
+++ internal/app/run.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"github.com/go-chi/chi/v5"
@@ -11,8 +12,12 @@ import (
 	"github.com/vancho-go/url-shortener/internal/app/storage"
 	"github.com/vancho-go/url-shortener/pkg/logger"
 	"go.uber.org/zap"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"path"
+	"syscall"
 )
 
 // Run запускает приложение.
@@ -35,6 +40,15 @@ func Run() error {
 
 	logger.Log.Info("Configuring http compress middleware")
 	compressMiddleware := compress.GzipMiddleware
+
+	// через этот канал сообщим основному потоку, что соединения закрыты
+	idleConnsClosed := make(chan struct{})
+	// канал для перенаправления прерываний
+	// поскольку нужно отловить всего одно прерывание,
+	// ёмкости 1 для канала будет достаточно
+	sigint := make(chan os.Signal, 1)
+	// регистрируем перенаправление прерываний
+	signal.Notify(sigint, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
 	r := chi.NewRouter()
 
@@ -59,19 +73,44 @@ func Run() error {
 
 	r.Mount("/debug", handlers.PprofHandler())
 
+	srv := &http.Server{
+		Addr:    configuration.ServerHost,
+		Handler: r,
+	}
+
+	// запускаем горутину обработки пойманных прерываний
+	go func() {
+		<-sigint
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		// сообщаем основному потоку,
+		// что все сетевые соединения обработаны и закрыты
+		close(idleConnsClosed)
+	}()
+
 	if configuration.EnableHTTPS {
 		logger.Log.Info("Starting https server", zap.String("address", configuration.ServerHost))
-		err = http.ListenAndServeTLS(configuration.ServerHost, path.Join("certs", "cert.pem"), path.Join("certs", "key.pem"), r)
-		if err != nil {
+		err = srv.ListenAndServeTLS(path.Join("certs", "cert.pem"), path.Join("certs", "key.pem"))
+		if !errors.Is(err, http.ErrServerClosed) {
 			return errors.New("error starting https server")
 		}
+
 	} else {
 		logger.Log.Info("Starting http server", zap.String("address", configuration.ServerHost))
-		err = http.ListenAndServe(configuration.ServerHost, r)
-		if err != nil {
+		err = srv.ListenAndServe()
+		if !errors.Is(err, http.ErrServerClosed) {
 			return errors.New("error starting http server")
 		}
 	}
+
+	// ждём завершения процедуры graceful shutdown
+	<-idleConnsClosed
+	// получили оповещение о завершении
+	// здесь можно освобождать ресурсы перед выходом,
+	// например закрыть соединение с базой данных,
+	// закрыть открытые файлы
+	fmt.Println("Server Shutdown gracefully")
 
 	return nil
 }
